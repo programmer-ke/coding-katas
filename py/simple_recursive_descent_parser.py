482e816acd0d112d3e81a5db5f8f1cb5baaa55b8
Kenny Rachuonyo <krmboya@gmail.com>
2022-06-12 13:31:40 +0300

    works end to end but has bug

--- py/simple_recursive_descent_parser.py
+++ py/simple_recursive_descent_parser.py
@@ -1,6 +1,6 @@
 """
 A simple recursive descent parser for basic arithmetic using
-s-expressions as in lisp.
+s-expressions (as in lisp).
 
 e.g.
 
@@ -15,25 +15,27 @@ e.g.
 
 
 Note that a language like this uses a prefix (aka Polish) notation 
-where the operator precedes it's operands.
+where the operator precedes its operands.
 
 For example while the infix (conventional) notation would be represented as 
 
 > 3 + 4
 7
 
-Prefix notation would be represented as
+prefix notation would be represented as
 
 > (+ 3 4)
 7
 
-The operator (binary in our scope) is followed by 2 or more operands.
+The operator (binary in this scope) is followed by 2 or more operands.
+
 For example
 
 > (+ 3 4 5)
 12
 
-This is equivalent to the following in infix notation
+is equivalent to the following in infix notation
+
 > (3 + 4) + 5
 12
 
@@ -43,19 +45,19 @@ additional operand as seen above, until all the operands are exhausted.
 
 We begin by a formal definition of our grammer.
 
-expr     ::= NUM
-         |   form
+expr              ::= NUM
+                  |   form
 
-form     ::= ( operator operands )
+form              ::= ( operator operands )
 
-operands ::= expr expr optional
+operands          ::= expr expr optional_operands
          
-optional ::= expr optional | none <-- todo: this eliminates left recursion but possibly adds extra condition
+optional_operands ::= expr optional_operands | None
 
-operator ::= +
-         |   -
-         |   *
-         |   /
+operator          ::= +
+                  |   -
+                  |   *
+                  |   /
 
 The definitions on the left are expanded to those on the right.
 This reads as:
@@ -63,9 +65,10 @@ This reads as:
 - An expression is either a number or a form.
 - A form is composed of an operator followed by operands enclosed in
   brackets
-- An operand is either a pair of expressions, or a set of operands
-  followed by an expression. This allows us to have 2 or more operands
-  in the form.
+- Operands are a pair of expressions followed by an optional operand
+- An optional operand is an expression followed by another optional
+  operand, or None. This allows us to accept any number of optional
+  operands
 - An operator is one of +, -, /, *
 
 NUM and the operators above are terminal symbols, meaning that they 
@@ -108,7 +111,7 @@ def generate_tokens(text):
     for m in master_pattern.finditer(text):
         token = Token(m.lastgroup, m.group())
         if token.type == "MISMATCH":
-            raise SyntaxError(f"Unexpected token {token.value}")
+            raise RuntimeError(f"Unexpected token {token.value}")
         elif token.type != "WHITESPACE":
             yield token
 
@@ -117,16 +120,35 @@ class Evaluator:
     """Parser for our expressions
 
     Provides a method `parse` which when given input string,
-    parses it and executes it giving a result
+    parses it and executes it returns a result
+
+    >>> evaluator = Evaluator()
+    >>> evaluator.parse("(+ 1 2)")
+    3
+
+    Key helper methods that are used:
+
+    _advance - Move forward one token
+    _accept  - If the next token is of the given type, advance and 
+               return True
+    _expect  - Advance expecting the next token to be of the given type
+               If it isn't, raise a syntax error
     """
 
     def parse(self, input_string):
         """Given an input string, evaluate it"""
 
         self.tokens = generate_tokens(input_string)
+
+        # We have a reference to both the current and next token
+        # This allows us to look ahead one token
+        self.token, self.next_token = None, None
+        self._advance()
         
-        # root our grammar is expr, so call that first
-        self.expr()
+        # root our grammar is expr
+        # call that and return result
+        result = self.expr()
+        return result
 
     def expr(self):
         """expr ::= NUM | form
@@ -136,7 +158,7 @@ class Evaluator:
         if self._accept("NUM"):
             value = int(self.token.value)
         else:
-            # 'descend' into form
+            # descend into form
             value = self.form()
         return value
 
@@ -153,8 +175,8 @@ class Evaluator:
 
         # operands should be a list of two or more operands
         # apply the operator on the operands
-        operand1, operand2, *rest = operands
-        result = self._apply(operator, operand1, operand2)
+        first, second, *rest = operands
+        result = self._apply(operator, first, second)
         for operand in rest:
             result = self._apply(operator, result, operand)
 
@@ -171,22 +193,85 @@ class Evaluator:
                 self._accept("TIMES") or 
                 self._accept("DIVIDE")
         ):
-            return self.token.type
+            return self.token.value
         
         raise SyntaxError("Expected PLUS or MINUS or TIMES or DIVIDE")
 
     def operands(self):
-        """operands ::= expr expr | operands expr
+        """operands ::= expr expr optional_operands
         
-        Operands are either 2 consecutive expressions, or
-        at least two consecutive expressions followed by
-        an expression"""
+        operands are two expressions followed by an optional operand
+        """
 
-        # todo: handle left recursion in this case
+        # recursively call expression twice
+        # Then add optional operands
+        first = self.expr()
+        second = self.expr()
 
+        return [first, second] + self.optional_operands()
         
-
+    def optional_operands(self):
+        """optional_operands ::= expr optional_operands | None
         
+        An expression followed by an optional operand, or None
+        """
+        
+        operands = []
 
-            
-            
+        # Check if the next token is the first token of an expression
+        # i.e. if token is in FIRST(expr) in parsing terminology
+        # If so, collect it and move to the next optional token
+        # If the check fails, we have hit the terminal condition (None)
+
+        FIRST_EXPR = ["NUM", "LEFT_PAREN"]  # FIRST(expr)
+
+        if self.next_token in FIRST_EXPR:
+            operands.append(self.expr())
+            operands += self.optional_operands()
+
+        return operands
+
+    def _apply(self, operator, left, right):
+        """Applies operator on the left and right operands"""
+
+        if operator == "+":
+            result = left + right
+        elif operator == "-":
+            result = left - right
+        elif operator == "*":
+            result = left * right
+        else:
+            result = left / right
+        return result
+
+    def _advance(self):
+        """Advances one token ahead in input stream"""
+
+        self.token, self.next_token = self.next_token, next(self.tokens, None)
+
+    def _accept(self, token_type):
+        """Advances to the next token if it matches the given type"""
+
+        if self.next_token.type == token_type:
+            self._advance()
+            return True
+        return False
+
+    def _expect(self, token_type):
+        """Advances to the next token expecting that it is of the given type
+
+        If the token is not of the given type, raises a syntax error
+        """
+
+        if not self._accept(token_type):
+            raise SyntaxError(f"Expected {token_type}")
+
+
+evaluator = Evaluator()
+assert evaluator.parse("1") == 1
+assert evaluator.parse("(+ 1 2)") == 3
+assert evaluator.parse("(- 1 2)") == -1
+assert evaluator.parse("(* 5 2 3)") == 10  # todo: fails, fix
+assert evaluator.parse("(/ 10 2)") == 5
+
+
