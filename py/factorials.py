6c501d9d2d3add034c3c6befb39d130c10aa2aa7
Kenny Rachuonyo <krmboya@gmail.com>
2022-08-14 14:55:29 +0300

    some cleanup of naive solution

--- py/factorials.py
+++ py/factorials.py
@@ -1,10 +1,8 @@
 """
-A factorial is a number n such that
-n >= 1 and
-there exists an integer x >= 0 such that x! = n and
-where x = 0, x! = 1 and
-where x > 0, x! = x * (x - 1)! and
-where n > 1, n % 2 == 0
+A factorial is a number x such that
+n! = x, where n is an integer >= 0 and
+0! = 1 and
+n! = n * (n - 1) * (n - 2) * ... * 0! for n >= 1
 """
 
 def test(is_factorial):
@@ -16,12 +14,10 @@ def test(is_factorial):
     for num in less_than_1:
         assert is_factorial(num) == False
 
-    # Any integer > 1 has to be even to be a factorial
-    assert is_factorial(5) == False
-
     # We can now test with known values
     assert is_factorial(2) == True # 2!
     assert is_factorial(5040) == True # 7!
+    assert is_factorial(5) == False
     assert is_factorial(892) == False
     assert is_factorial(10000) == False
 
@@ -29,33 +25,29 @@ def test(is_factorial):
 def is_factorial_naive(num):
     """Returns a boolean indicating whether num is a factorial"""
 
+    if num < 1:
+        # n >= 0 and 0! = 1
+        return False
+
     if num == 1:
-        # x = 0 and x! = 1 and (x + 1)! = 1 * 0! = 1
+        # (n = 0 and 0! = 1) or (n = 1 and n! = 1 * !0)
         return True
 
-    if num < 1:
-        # x >= 0 and 0! = 1 and x! >= 1
-        return False
-
-    if num > 1 and num % 2 != 0:
-        # x > 1 and x! > 1 and x! = x * (x - 1) * ... * 2 * 1!
-        return False
-
-    # 1! = 1 and x = 1
-    x = 1
+    # n = 1 and fct = 1! = 1
+    n = 1
     fct = 1
 
     while fct < num:
-        # loop invariant: x! = fct and (x - 1)! < num
-        x += 1
-        fct *= x
+        # loop invariant: n! = fct and (n - 1)! < num
+        n += 1
+        fct *= n
         
     
     if fct == num:
-        # x! = num
+        # n! = num
         return True
 
-    # x! > num
+    # n! > num
     return False
 
 
