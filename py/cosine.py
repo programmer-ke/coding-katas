9a53d8f4209aace631b38da204394ddb3a945a54
Kenny Rachuonyo <krmboya@gmail.com>
2022-08-23 11:21:10 +0300

    implemented cos

--- py/cosine.py
+++ py/cosine.py
@@ -23,8 +23,8 @@ Each term after 1 in the series can be described as
 
 (x^i)/i! for i in the series 2, 4, 6, 8, ...
 
-We also not that the sign alternates with each additional term in the
-following way with each additional sequence
+We also note that the sign alternates with each additional term in the
+sequence in following way:
 
 sign = -sign
 
@@ -64,6 +64,47 @@ base case = 1
 3rd term: x^6/6! = x/6 * x/5 * x^4/4! = x^2/(6 * 5) * 2nd term
 ... and so forth.
 
-We can therefore represent the a subsequent term in the series as
+We can therefore represent the a subsequent term in the series in terms
+of the previous term as:
+
 x^i/i! = (x^2/i*(i-1)) * previous term
+
+Time complexity for each additional time now remains constant.
+
+Putting all the above together, our general approach is to start
+with 1 as our base term, then iteratively generate each additional
+term negating the sign and adding it to the previous. We do this until
+the absolute value is less than the acceptable error of 10^-6.
+At this point, the sum of terms will be an accurate enough cosine of
+our input.
 """
+
+import math
+
+def main():
+    inputs = [-1, 0, 1]
+    for x in inputs:
+        assert math.isclose(math.cos(x), cosine(x), rel_tol=1e-06)
+
+
+def cosine(x):
+    """Returns the cosine of x to an acceptable error of 10^-6
+
+    Uses the taylor series, but refactored:
+    cos(x) = 1 - (x^2)/2! + (x^4)/4! - (x^6)/6! + ...
+    """
+
+    y = 0
+    term = 1
+    i = 0  # holds next in the series 0, 2, 4, 6, ...
+    x2 = x * x  # x^2 is constant in each iteration
+
+    while abs(term) > 1e-06:
+        # loop invariant:
+        # for iteration j, i = 2j and y = sum of first j terms in series
+        y += term
+        i += 2
+        term = -(x2 / (i * (i - 1)) * term)
+    return y
+
+main()
