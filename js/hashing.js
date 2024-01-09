ff43fba97e0e20fee501057b72b36eb817d092a5
Kenny Rachuonyo <krmboya@gmail.com>
2024-01-09 09:59:59 +0300

    minor updates

--- js/hashing.js
+++ js/hashing.js
@@ -4,9 +4,13 @@ const hashFunction = (input_text) => {
     const hash = createHash('sha256');
     hash.update(input_text);
     return hash.digest('hex');
+
+    let variableLengthInput = "The quick brown fox jumps over the lazy dog";
+    let fixedLengthHash = hashFunction(variableLengthInput);
+
+    console.log(fixedLengthHash);
+    // outputs: d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592
+
+    console.log(fixedLengthHash.length);
+    // outputs: 64
 };
-
-
-let variableLengthInput = "The fox jumped over the wall";
-let fixedLengthOutput = hashFunction(variableLengthInput);
-console.log(fixedLengthOutput);
--- py/hashing.py
+++ py/hashing.py
@@ -1,16 +1,16 @@
-"""
-Hashing in python
-
-digraph {
-   input [label = "Variable-Length Input"];
-   output [lable = "Fixed-Length Output"]
-   input -> "hash function" -> output
-}
-"""
+"""Hashing in python"""
 
 import hashlib
 
-variable_length_input = "The fox jumped over the wall"
+variable_length_input = "The quick brown fox jumps over the lazy dog"
+
+fixed_length_hash = hash_function(variable_length_input)
+
+print(fixed_length_hash)
+# outputs: d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592
+
+print(len(fixed_length_hash))
+# outputs: 64
 
 def hash_function(input_text):
     """Returns a fixed length hash of the input"""
@@ -18,5 +18,3 @@ def hash_function(input_text):
     input_bytes = bytes(input_text, encoding="utf-8")
     return hashlib.sha256(input_bytes).hexdigest()
 
-fixed_length_output = hash_function(input_text)
-len(fixed_length_output)
